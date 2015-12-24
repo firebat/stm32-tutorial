6eb6d4f310a369fd1f9b685e73f082c91d62b97d
张中原 <zhongyuan.zhang@qunar.com>
2015-12-24 21:33:59 +0800

    fix usart

--- 5_usart/main.c
+++ 5_usart/main.c
@@ -1,40 +1,9 @@
 #include <stm32f10x.h>
 
-void RCC_Configuration(void);
-void GPIO_Configuration(void);
-void USART_Configuration(void);
-
-
-void putc(char c)
-{
-  USART_SendData(USART1, c);
-  while(USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET);
-}
-
-void puts(char* s)
-{
-  while(*s) {
-    putc(*s++);
-  }
-}
-
-int main(void)
-{
-  RCC_Configuration();
-  GPIO_Configuration();
-  USART_Configuration();
-
-  while(1) {
-    puts("Hello world!\r\n");
-  }
-}
-
-
 void RCC_Configuration(void)
 {
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1
-                         | RCC_APB2Periph_GPIOA
-                         | RCC_APB2Periph_AFIO, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 }
 
 
@@ -56,7 +25,7 @@ void GPIO_Configuration(void)
 void USART_Configuration(void)
 {
   USART_InitTypeDef usart_init;
-  usart_init.USART_BaudRate = 115200;
+  usart_init.USART_BaudRate = 9600;
   //  usart_init.USART_WordLength = USART_WordLength_8b;
   //  usart_init.USART_StopBits = USART_StopBits_1;
   //  usart_init.USART_Parity = USART_Parity_No;
@@ -66,3 +35,23 @@ void USART_Configuration(void)
 
   USART_Cmd(USART1, ENABLE);
 }
+
+void USART_Write(USART_TypeDef *usart, char *s)
+{
+  while(*s) {
+    USART_SendData(usart, *s++);
+    while(USART_GetFlagStatus(usart, USART_FLAG_TC) == RESET);
+  }
+}
+
+int main(void)
+{
+  RCC_Configuration();
+  GPIO_Configuration();
+  USART_Configuration();
+
+  while(1) {
+    USART_Write(USART1, "Welcome!\r\n");
+  }
+}
+
